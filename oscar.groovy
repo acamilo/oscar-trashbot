47cb726ed70ac054c71dbd1d685818a3dbf33b75
acamilo <alex.camilo@gmail.com>
2015-10-18 16:40:39 -0400

    Switching from gist to real repo

---
+++ oscar.groovy
@@ -0,0 +1,20 @@
+print "hello world "
+BowlerStudio.speak("Welcome to Bowler Studio");
+
+ServoChannel luremouth = new ServoChannel (dyio.getChannel(1));
+ServoChannel lureeyes = new ServoChannel (dyio.getChannel(1));
+
+DigitalOutputChannel arm  = new DigitalOutputChannel(dyio.getChannel(23)); // arm
+DigitalOutputChannel mouth  = new DigitalOutputChannel(dyio.getChannel(22)); // mouth
+DigitalOutputChannel head = new DigitalOutputChannel(dyio.getChannel(21)); // head
+DigitalOutputChannel claw   = new DigitalOutputChannel(dyio.getChannel(20)); // claw
+
+def popup = {head.setHigh(true); arm.setHigh(true);}
+def hide = {head.setHigh(false); arm.setHigh(false); mouth.setHigh(false); }
+def growl = {mouth.setHigh(true); Runtime.getRuntime().exec("mpg123 /home/teacher/git/7dd828fa3e945ade2bf3/growl.mp3");}
+
+sleep(1000)
+popup()
+growl()
+sleep(5000)
+hide()
